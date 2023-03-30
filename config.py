7f20042818cf252cdedc4ab622e8f317356190fc
Maryam Riaz <riazmaryam910@gmail.com>
2023-03-31 01:38:45 +0500

    Refactored Code

---
+++ config.py
@@ -0,0 +1,9 @@
+# Oracle database configuration
+PATH_TO_ORACLE_CLIENT = "C:\\oracle\\"
+USERNAME = "hr"
+PASSWORD = "12345678@"
+HOST = "localhost"
+PORT = 1521
+DATABASE = "xe"
+
+CONNECTION_STRING = f"{USERNAME}/{PASSWORD}//{HOST}:{PORT}/{DATABASE}"
--- main.py
+++ main.py
@@ -1,69 +1,63 @@
 import cx_Oracle
+import config
 
-PATH_TO_ORACLE_CLIENT = "C:\\oracle\\"
-cx_Oracle.init_oracle_client(lib_dir=PATH_TO_ORACLE_CLIENT) 
-
-USERNAME = "hr"
-PASSWORD = "12345678@"
-HOST = "localhost"
-PORT = 1521
-DATABASE = "xe"
-
-CONNECTION_STRING = f"{USERNAME}/{PASSWORD}//{HOST}:{PORT}/{DATABASE}"
-
-conn=cx_Oracle.connect(CONNECTION_STRING)
+cx_Oracle.init_oracle_client(lib_dir=config.PATH_TO_ORACLE_CLIENT) 
+conn=cx_Oracle.connect(config.CONNECTION_STRING)
 print("Orcale, is connected to ",conn.version, " version of database.")
 
 cur=conn.cursor()
 
+def insert_data(username, last_name):
+    query = f"""INSERT INTO STUDENT_DETAILS(FIRST_NAME,LASTNAME) values('{username}','{last_name}')"""
+    cur.execute(query)
+    conn.commit()
+
+def show_data():
+    query = "select * from STUDENT_DETAILS"
+    cur.execute(query)
+    if not cur:
+        print("No records found")
+        return
+    for idx, row in enumerate(cur):
+        first_name = row[0]
+        last_name = row[1]
+        print(f"{idx}| {first_name} {last_name}")
+
+def delete_user(user_id):
+    query = f"delete from STUDENT_DETAILS where LASTNAME= '{user_id}'"
+    cur.execute(query)
+    conn.commit()
+
+print("""
+Welcome to the Airline Database Manager
+Options:
+- 1 | insert data
+- 2 | display data
+- 3 | delete data
+- 4 | update data
+- 5 | search data
+""")
+
 while True:
-    print('welcome! to the program.')
-    print()
-    print('PRESS 1 to insert the data\n')
-    print('PRESS 2 to display the data\n')
-    print('PRESS 3 to delete the data\n')
-    print('PRESS 4 to update the data\n')
-    print('PRESS 5 to search the data\n')
-    print()
+
     try:
         choice = int(input('Enter the choice '))
 
         if choice == 1:
             # insert
-            username = input('Enter your name. ')
-            phone = input('Enter your phone number. ')
-            query = "insert into STUDENT_DETAILS(FIRST_NAME,LAST_NAME) values('{}','{}')".format(username, phone)
-            # print(query)
-            cur.execute(query)
-            conn.commit()
-            print("data",username,phone,"saved to db")
-            input()
-        elif choice == 2:
-            # display data
-            query = "select * from STUDENT_DETAILS"
+            username = input('Enter your name: ')
+            last_name = input('Enter your last name: ')
+            insert_data(username=username, last_name=last_name)
+            print(f"data to database!")
 
-            # print(query)
-            cur.execute(query)
-            print('---------------------------')
-            postion=1
-            for row in cur:
-                print('Record no.',postion)
-                srno=1
-                print('|',srno,"FIRST_NAME : ", row[0],'|')
-                print('|',srno+1,"LAST_NAME  : ", row[1],'|')
-                postion=postion+1
-                #print('***************************')
-            print('---------------------------')
-            input()
+        elif choice == 2:
+            show_data()
+
         elif choice == 3:
             # delete the data
-            delete_RecordId=input('Enter the record id you want to delete.')
-            query = "delete from STUDENT_DETAILS where LAST_NAME= '{}'".format(delete_RecordId)
-            #print(query)
-            cur.execute(query)
-            conn.commit()
-            print("data id", delete_RecordId, "deleted from db")
-            input()
+            delete_RecordId=input('Enter the record id you want to delete: ')
+            delete_user(user_id=delete_RecordId)
+
         elif choice == 4:
             # update
             pass
